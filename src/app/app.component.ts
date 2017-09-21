f8a0a016561eef110de10cd8e4b24ef79e30ae56
Thomas Watson <thomaswatson@Thomass-MacBook-Pro.local>
2017-09-21 15:15:26 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,170 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, NgZone } from '@angular/core';
+import * as Quagga from 'quagga';
+import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  template: `<button (click)="openScanner()">Scan</button>
+  <modal (onDismiss)="close()" #Modal>
+  <modal-header [show-close]="true">
+      <h4 class="modal-title">Scan your barcode</h4>
+  </modal-header>
+  <modal-body>
+  <div id="interactive" class="viewport clearfix" #interactive></div>
+  </modal-body>
+</modal>
+              Code: {{output}}`
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild('Modal') modal: ModalComponent;
+  CameraIsAvailable: boolean = false;
+  quaggaConfig: any;
+  output: string;
+  html: string;
+
+  constructor(private zone: NgZone) {
+    this.quaggaConfig = {
+      locator: {
+        patchSize: 'medium',
+        halfSample: false,
+        boxFromPatches: {
+          showTransformed: true,
+          showTransformedBox: true,
+          showBB: true
+        }
+      },
+      inputStream: {
+        type: 'LiveStream',
+        constraints: {
+          width: {
+            min: 400
+          },
+          height: {
+            min: 400
+          },
+          facingMode: 'environment',
+          aspectRatio: {
+            min: 1,
+            max: 2
+          }
+        }
+      },
+      numOfWorkers: 1,
+      decoder: {
+        readers: [
+          'ean_reader',
+          'code_128_reader',
+          'ean_8_reader',
+          'code_39_reader',
+          'code_39_vin_reader',
+          'codabar_reader',
+          'upc_reader',
+          'upc_e_reader',
+          'i2of5_reader'
+        ]
+      },
+      locate: true
+    };
+  }
+
+  openScanner() {
+    this.open();
+    this.CameraIsAvailable = navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia === 'function';
+    if (this.CameraIsAvailable) {
+      Quagga.init(this.quaggaConfig, (err: any) => {
+        if (err) {
+          throw new Error(err);
+        }
+        Quagga.start();
+      });
+    }
+    this.onProcessed();
+    this.onDecodeDetected();
+  }
+  // Open and close methods for the modal
+  open() {
+    this.modal.open();
+  }
+  close() {
+    this.modal.close();
+    Quagga.stop();
+  }
+  onProcessed() {
+    Quagga.onProcessed(result => {
+      const drawingCtx = Quagga.canvas.ctx.overlay,
+        drawingCanvas = Quagga.canvas.dom.overlay;
+      if (result) {
+        if (result.boxes) {
+          drawingCtx.clearRect(
+            0,
+            0,
+            parseInt(drawingCanvas.getAttribute('width')),
+            parseInt(drawingCanvas.getAttribute('height'))
+          );
+          result.boxes
+            .filter(function(box) {
+              return box !== result.box;
+            })
+            .forEach(function(box) {
+              Quagga.ImageDebug.drawPath(
+                box,
+                {
+                  x: 0,
+                  y: 1
+                },
+                drawingCtx,
+                {
+                  color: 'green',
+                  lineWidth: 2
+                }
+              );
+            });
+        }
+        if (result.box) {
+          Quagga.ImageDebug.drawPath(
+            result.box,
+            {
+              x: 0,
+              y: 1
+            },
+            drawingCtx,
+            {
+              color: '#00F',
+              lineWidth: 2
+            }
+          );
+        }
+
+        if (result.codeResult && result.codeResult.code) {
+          Quagga.ImageDebug.drawPath(
+            result.line,
+            {
+              x: 'x',
+              y: 'y'
+            },
+            drawingCtx,
+            {
+              color: 'red',
+              lineWidth: 3
+            }
+          );
+        }
+      }
+    });
+  }
+  onDecodeDetected() {
+    Quagga.onDetected(result => {
+      if (!result || typeof result.codeResult === 'undefined') {
+        console.log('Cannot be Detected, Please Try again!');
+      }
+
+      // Close the modal
+      this.close();
+
+      // Force interpolation on the template
+      this.zone.run(() => {
+        this.output = result.codeResult.code;
+      });
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { Ng2Bs3ModalModule } from 'ng2-bs3-modal/ng2-bs3-modal';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule, Ng2Bs3ModalModule],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
